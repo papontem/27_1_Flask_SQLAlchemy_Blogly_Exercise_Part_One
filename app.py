b90fb76a740d92b1c5de2bcfb291796a3fb300ca
papontemjetix <papontem@gmail.com>
2023-07-18 13:03:36 -0400

    bare bones model.py and app.py created

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, render_template, redirect, flash, session, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+#flask debugtoolbar settings
+app.config['SECRET_KEY'] = "oh-so-secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app) 
+
 connect_db(app)
 db.create_all()
+
+#ROUTES
+@app.route('/') 
+def homepage(): 
+    """Show homepage"""
+    return render_template("home.html")
